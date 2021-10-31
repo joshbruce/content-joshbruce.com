2fe9f917df2d4a463fddf81b65a3f40b5959a8d8
Josh Bruce <josh@8fold.pro>
2021-10-31 02:06:48 -0500

    remove js

--- assets/js/menu.js
+++
@@ -1,1 +0,0 @@
-window.onload=function(){document.querySelector("#main-nav-form > form > button").style.display="none",document.getElementById("change-page-select").onchange=function(e){const n=e.target,t=n.options[n.selectedIndex];window.location.pathname=t.getAttribute("value")}};
